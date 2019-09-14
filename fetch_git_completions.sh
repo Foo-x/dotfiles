c98da8e1c3536b1abf0a910faedfe14aa33fb853
Foo-x <www.sand.x@gmail.com>
2019-09-15 02:43:35 +0900

    Add hub completion

--- fetch_git_completions.sh
+++ fetch_git_completions.sh
@@ -3,3 +3,5 @@
 wget https://github.com/git/git/raw/master/contrib/completion/git-prompt.sh
 wget https://github.com/git/git/raw/master/contrib/completion/git-completion.bash
 echo "__git_complete g __git_main" >> git-completion.bash
+
+wget https://github.com/github/hub/raw/master/etc/hub.bash_completion.sh
