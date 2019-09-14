b13a2ced963e893fc60d2c74501ff070ca04e9e4
Foo-x <www.sand.x@gmail.com>
2019-09-15 02:35:09 +0900

    Update git completion

--- fetch_git_completions.sh
+++ fetch_git_completions.sh
@@ -2,3 +2,4 @@
 
 wget https://github.com/git/git/raw/master/contrib/completion/git-prompt.sh
 wget https://github.com/git/git/raw/master/contrib/completion/git-completion.bash
+echo "__git_complete g __git_main" >> git-completion.bash
