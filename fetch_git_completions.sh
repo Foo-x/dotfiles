2bf04b323cb6371ff3f478037f0fde8ef8457fea
Foo-x <www.sand.x@gmail.com>
2021-06-18 01:13:09 +0900

    chore: fix curl options

--- fetch_git_completions.sh
+++ fetch_git_completions.sh
@@ -7,14 +7,14 @@ fi
 cd $(dirname ${BASH_SOURCE:-$0})
 
 if [[ ! -f git-prompt.sh ]]; then
-    curl -O https://github.com/git/git/raw/master/contrib/completion/git-prompt.sh
+    curl -fsSLO https://github.com/git/git/raw/master/contrib/completion/git-prompt.sh
 fi
 
 if [[ ! -f git-completion.bash ]]; then
-    curl -O https://github.com/git/git/raw/master/contrib/completion/git-completion.bash
+    curl -fsSLO https://github.com/git/git/raw/master/contrib/completion/git-completion.bash
     echo "__git_complete g __git_main" >> ${SCRIPT_DIR}/git-completion.bash
 fi
 
 if [[ ! -f hub.bash_completion.sh ]]; then
-    curl -O https://github.com/github/hub/raw/master/etc/hub.bash_completion.sh
+    curl -fsSLO https://github.com/github/hub/raw/master/etc/hub.bash_completion.sh
 fi
