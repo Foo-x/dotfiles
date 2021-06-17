c6d2e737df09f6fcac0bea017be3055c6ba0b284
Foo-x <www.sand.x@gmail.com>
2021-06-17 22:54:44 +0900

    chore: fix wget to curl

--- fetch_git_completions.sh
+++ fetch_git_completions.sh
@@ -1,20 +1,20 @@
 #!/bin/bash
 
 if ! type git &> /dev/null; then
-    return
+    exit 0
 fi
 
-SCRIPT_DIR=$(cd $(dirname ${BASH_SOURCE:-$0}); pwd)
+cd $(dirname ${BASH_SOURCE:-$0})
 
-if [[ ! -f ${SCRIPT_DIR}/git-prompt.sh ]]; then
-    wget -P ${SCRIPT_DIR} https://github.com/git/git/raw/master/contrib/completion/git-prompt.sh
+if [[ ! -f git-prompt.sh ]]; then
+    curl -O https://github.com/git/git/raw/master/contrib/completion/git-prompt.sh
 fi
 
-if [[ ! -f ${SCRIPT_DIR}/git-completion.bash ]]; then
-    wget -P ${SCRIPT_DIR} https://github.com/git/git/raw/master/contrib/completion/git-completion.bash
+if [[ ! -f git-completion.bash ]]; then
+    curl -O https://github.com/git/git/raw/master/contrib/completion/git-completion.bash
     echo "__git_complete g __git_main" >> ${SCRIPT_DIR}/git-completion.bash
 fi
 
-if [[ ! -f ${SCRIPT_DIR}/hub.bash_completion.sh ]]; then
-    wget -P ${SCRIPT_DIR} https://github.com/github/hub/raw/master/etc/hub.bash_completion.sh
+if [[ ! -f hub.bash_completion.sh ]]; then
+    curl -O https://github.com/github/hub/raw/master/etc/hub.bash_completion.sh
 fi
