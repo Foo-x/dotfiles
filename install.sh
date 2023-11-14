c016b70f01ee1e9e77895794b3198508e7f876cf
Foo-x <www.sand.x@gmail.com>
2023-11-15 02:57:24 +0900

    chore: separate setup vim

--- install.sh
+++ install.sh
@@ -70,21 +70,6 @@ if [ ! -d ${HOME}/enhancd ]; then
 fi
 
 mkdir -p ${HOME}/.local/bin
-# install neovim
-if [ ! -f ${HOME}/.local/bin/nvim ]; then
-    curl -L https://github.com/neovim/neovim/releases/download/stable/nvim.appimage > ${HOME}/.local/bin/nvim.appimage
-    chmod +x ${HOME}/.local/bin/nvim.appimage
-    if ${HOME}/.local/bin/nvim.appimage -v > /dev/null 2>&1; then
-        mv ${HOME}/.local/bin/nvim.appimage ${HOME}/.local/bin/nvim
-    else
-        (cd ${HOME}/.local/bin/; ./nvim.appimage --appimage-extract; ln -sf ./squashfs-root/usr/bin/nvim ./nvim; rm ./nvim.appimage) > /dev/null 2>&1
-    fi
-fi
-
-binfiles="
-nvimdirdiff
-"
-echo "${binfiles}" | xargs -I{} ln -sf ${DOT_DIR}/bin/{} ${HOME}/.local/bin/{}
 
 touch ${HOME}/.user_profile
 
@@ -112,47 +97,6 @@ prepare-commit-msg
 "
 echo "${githooks_files}" | xargs -I{} ln -sf ${DOT_DIR}/.config/git/hooks/{} ${XDG_CONFIG_HOME}/git/hooks/{}
 
-# setup vim
-mkdir -p ${XDG_CONFIG_HOME}/vim
-mkdir -p ${XDG_CONFIG_HOME}/vim/pack/plugins/start/
-mkdir -p ${XDG_CONFIG_HOME}/vim/pack/colors/start/
-vimrc_files=$(cd ${DOT_DIR}/.config/vim && \ls -1 *vimrc)
-printf "${vimrc_files}" | xargs -I{} ln -sf ${DOT_DIR}/.config/vim/{} ${XDG_CONFIG_HOME}/vim/{}
-
-VIM_PACK_DIR=${XDG_CONFIG_HOME}/vim/pack/plugins/start
-# install fzf.vim
-if [ ! -d ${VIM_PACK_DIR}/fzf.vim ]; then
-    git clone --depth 1 https://github.com/junegunn/fzf.vim.git ${VIM_PACK_DIR}/fzf.vim
-fi
-
-# install vim-fugitive
-if [ ! -d ${VIM_PACK_DIR}/vim-fugitive ]; then
-    git clone --depth 1 https://github.com/tpope/vim-fugitive.git ${VIM_PACK_DIR}/vim-fugitive
-fi
-
-# install gv.vim
-if [ ! -d ${VIM_PACK_DIR}/gv.vim ]; then
-    git clone --depth 1 https://github.com/junegunn/gv.vim.git  ${VIM_PACK_DIR}/gv.vim
-fi
-
-# install vim-gitgutter
-if [ ! -d ${VIM_PACK_DIR}/vim-gitgutter ]; then
-    git clone --depth 1 https://github.com/airblade/vim-gitgutter.git ${VIM_PACK_DIR}/vim-gitgutter
-fi
-
-# install vimdoc-ja
-if [ ! -d ${VIM_PACK_DIR}/vimdoc-ja ]; then
-    git clone --depth 1 https://github.com/vim-jp/vimdoc-ja.git ${VIM_PACK_DIR}/vimdoc-ja
-fi
-
-mkdir -p ${XDG_CONFIG_HOME}/nvim
-nvim_files="
-init.vim
-"
-echo "${nvim_files}" | xargs -I{} ln -sf ${DOT_DIR}/.config/nvim/{} ${XDG_CONFIG_HOME}/nvim/{}
-
-nvim +'helptags ALL' +qa
-
 # setup tmux
 mkdir -p ${XDG_CONFIG_HOME}/tmux
 ln -sf ${DOT_DIR}/.config/tmux/tmux.conf ${XDG_CONFIG_HOME}/tmux/tmux.conf
@@ -169,5 +113,7 @@ if [ -f ${HOME}/.bash_history ]; then
   cat ${HOME}/.bash_history >> ${XDG_STATE_HOME}/bash/history
   rm ${HOME}/.bash_history
 fi
+
+sh ${DOT_DIR}/setup_vim.sh
 
 echo "Done."
