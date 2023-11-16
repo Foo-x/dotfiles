7e3c5784d2705001c3f55f4bf92d21be8610e392
Foo-x <www.sand.x@gmail.com>
2023-11-17 03:12:50 +0900

    chore: update vimrc

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -12,34 +12,62 @@ require('fidget').setup()
 mason.setup()
 mason_lspconfig.setup {
   ensure_installed = {
-    'tsserver',
+    'bashls',
+    'cssls',
+    'cssmodules_ls',
+    'dockerls',
+    'docker_compose_language_service',
     'eslint',
-    'lua_ls',
-    'vimls',
+    'intelephense',
+    'html',
     'jsonls',
+    'lua_ls',
+    'marksman',
+    'pyright',
+    'sqlls',
+    'tsserver',
+    'vimls',
     'yamlls',
   },
 }
 
 mason_lspconfig.setup_handlers {
   function(server)
+    local on_attach = function(client, bufnr)
+      if client.supports_method "textDocument/documentHighlight" then
+        vim.cmd [[
+        set updatetime=300
+        augroup lsp_document_highlight
+          autocmd!
+          autocmd CursorHold,CursorHoldI <buffer> lua vim.lsp.buf.document_highlight()
+          autocmd CursorMoved,CursorMovedI <buffer> lua vim.lsp.buf.clear_references()
+        augroup END
+        ]]
+      end
+    end
+
     local opts = {
+      on_attach = function(client, bufnr)
+        on_attach(client, bufnr)
+      end,
       capabilities = require('cmp_nvim_lsp').default_capabilities(),
     }
 
     lspconfig[server].setup(opts)
   end,
 }
-vim.cmd [[
-set updatetime=300
-augroup lsp_document_highlight
-  autocmd!
-  autocmd CursorHold,CursorHoldI * lua vim.lsp.buf.document_highlight()
-  autocmd CursorMoved,CursorMovedI * lua vim.lsp.buf.clear_references()
-augroup END
-]]
+
+local function help()
+  local ft = vim.opt.filetype._value
+  if ft == 'vim' or ft == 'help' then
+    vim.cmd([[execute 'h ' . expand('<cword>') ]])
+  else
+    require('lspsaga.hover').render_hover_doc()
+  end
+end
 
 local set = vim.keymap.set
+set('n', 'K', help)
 set('n', 'gi', vim.lsp.buf.implementation)
 set('n', '<F2>', require('lspsaga.rename').rename)
 -- TODO: set more keymap
@@ -55,6 +83,8 @@ cmp.setup {
     end,
   },
   mapping = cmp.mapping.preset.insert({
+    ['<C-j>'] = cmp.mapping.scroll_docs(4),
+    ['<C-k>'] = cmp.mapping.scroll_docs(-4),
     ['<C-Space>'] = cmp.mapping.complete(),
     ['<C-e>'] = cmp.mapping.abort(),
     ['<CR>'] = cmp.mapping.confirm({ select = true }),
@@ -90,8 +120,29 @@ cmp.setup.cmdline('/', {
 -- treesitter
 require('nvim-treesitter.configs').setup({
   ensure_installed = {
+    'bash',
+    'css',
+    'csv',
+    'dockerfile',
+    'git_config',
+    'gitcommit',
+    'gitignore',
+    'html',
+    'java',
+    'javascript',
+    'json',
+    'jsonc',
+    'lua',
+    'markdown',
+    'markdown_inline',
+    'php',
+    'python',
+    'regex',
+    'sql',
     'typescript',
+    'tsv',
     'tsx',
+    'vim',
   },
   highlight = {
     enable = true,
--- .config/vim/command.vimrc
+++ .config/vim/command.vimrc
@@ -14,17 +14,4 @@ if 1
   command! -nargs=+ -complete=file EditMultiple silent! argdelete * | AA <args> | TabArgs
 
   command! TabcloseRight +,$tabdo tabclose
-
-  " keywordprg
-  fun! s:KeywordHelp(keyword)
-    exe "help" a:keyword
-  endf
-  nnoremap <silent> KH :<C-u>call <SID>KeywordHelp(expand("<cword>"))<CR>
-  vnoremap KH "zy:call <SID>KeywordHelp(@z)<CR>
-
-  fun! s:KeywordGoogle(keyword)
-    exe "!open 'https://www.google.co.jp/search?q=" . a:keyword . "'"
-  endf
-  nnoremap KG :<C-u>call <SID>KeywordGoogle(expand("<cword>"))<CR><CR>
-  vnoremap KG "zy:call <SID>KeywordGoogle(@z)<CR><CR>
 endif
