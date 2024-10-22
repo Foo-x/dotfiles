3deb4bbbc74c706f62e9235521957a2b6162198b
Foo-x <www.sand.x@gmail.com>
2024-10-22 23:44:53 +0900

    perf: improve nvim performance

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -689,104 +689,110 @@ cmp.setup.cmdline(':', {
 -- }}}
 
 -- treesitter {{{
-if vim.fn.executable('cc') == 1 or vim.fn.executable('gcc') == 1 or vim.fn.executable('clang') == 1 or vim.fn.executable('cl') == 1 or vim.fn.executable('zig') == 1 then
-  require('nvim-treesitter.configs').setup({
-    ensure_installed = {
-      'bash',
-      'css',
-      'csv',
-      'dockerfile',
-      'git_config',
-      'gitcommit',
-      'gitignore',
-      'html',
-      'java',
-      'javascript',
-      'json',
-      'jsonc',
-      'lua',
-      'markdown',
-      'markdown_inline',
-      'php',
-      'python',
-      'regex',
-      'sql',
-      'typescript',
-      'tsv',
-      'tsx',
-      'vim',
-    },
-    textobjects = {
-      select = {
-        enable = true,
-        lookahead = true,
-        keymaps = {
-          ['af'] = '@function.outer',
-          ['if'] = '@function.inner',
-          ['as'] = { query = '@scope', query_group = 'locals' },
+vim.api.nvim_create_autocmd('CursorHold', {
+  group = vim.api.nvim_create_augroup('TSInit', {}),
+  once = true,
+  callback = function()
+    if vim.fn.executable('cc') == 1 or vim.fn.executable('gcc') == 1 or vim.fn.executable('clang') == 1 or vim.fn.executable('cl') == 1 or vim.fn.executable('zig') == 1 then
+      require('nvim-treesitter.configs').setup({
+        ensure_installed = {
+          'bash',
+          'css',
+          'csv',
+          'dockerfile',
+          'git_config',
+          'gitcommit',
+          'gitignore',
+          'html',
+          'java',
+          'javascript',
+          'json',
+          'jsonc',
+          'lua',
+          'markdown',
+          'markdown_inline',
+          'php',
+          'python',
+          'regex',
+          'sql',
+          'typescript',
+          'tsv',
+          'tsx',
+          'vim',
         },
-        include_surrounding_whitespace = true,
-      },
-      move = {
-        enable = true,
-        set_jumps = true,
-        goto_next_start = {
-          [']f'] = '@function.outer',
-          [']s'] = { query = '@scope', query_group = 'locals' },
-          [']z'] = { query = '@fold', query_group = 'folds' },
+        textobjects = {
+          select = {
+            enable = true,
+            lookahead = true,
+            keymaps = {
+              ['af'] = '@function.outer',
+              ['if'] = '@function.inner',
+              ['as'] = { query = '@scope', query_group = 'locals' },
+            },
+            include_surrounding_whitespace = true,
+          },
+          move = {
+            enable = true,
+            set_jumps = true,
+            goto_next_start = {
+              [']f'] = '@function.outer',
+              [']s'] = { query = '@scope', query_group = 'locals' },
+              [']z'] = { query = '@fold', query_group = 'folds' },
+            },
+            goto_next_end = {
+              [']F'] = '@function.outer',
+              [']S'] = { query = '@scope', query_group = 'locals' },
+              [']Z'] = { query = '@fold', query_group = 'folds' },
+            },
+            goto_previous_start = {
+              ['[f'] = '@function.outer',
+              ['[s'] = { query = '@scope', query_group = 'locals' },
+              ['[z'] = { query = '@fold', query_group = 'folds' },
+            },
+            goto_previous_end = {
+              ['[F'] = '@function.outer',
+              ['[S'] = { query = '@scope', query_group = 'locals' },
+              ['[Z'] = { query = '@fold', query_group = 'folds' },
+            },
+          },
         },
-        goto_next_end = {
-          [']F'] = '@function.outer',
-          [']S'] = { query = '@scope', query_group = 'locals' },
-          [']Z'] = { query = '@fold', query_group = 'folds' },
-        },
-        goto_previous_start = {
-          ['[f'] = '@function.outer',
-          ['[s'] = { query = '@scope', query_group = 'locals' },
-          ['[z'] = { query = '@fold', query_group = 'folds' },
-        },
-        goto_previous_end = {
-          ['[F'] = '@function.outer',
-          ['[S'] = { query = '@scope', query_group = 'locals' },
-          ['[Z'] = { query = '@fold', query_group = 'folds' },
-        },
-      },
-    },
-    highlight = {
-      enable = true,
-      disable = function(_, buf)
-        vim.b.large_buf = false
+        highlight = {
+          enable = true,
+          disable = function(_, buf)
+            vim.b.large_buf = false
 
-        -- check size
-        local max_filesize = 100 * 1024 -- 100 KB
-        local size = vim.fn.getfsize(vim.api.nvim_buf_get_name(vim.api.nvim_get_current_buf()))
-        if size > max_filesize then
-          vim.b.large_buf = true
-          return true
-        end
+            -- check size
+            local max_filesize = 100 * 1024 -- 100 KB
+            local size = vim.fn.getfsize(vim.api.nvim_buf_get_name(vim.api.nvim_get_current_buf()))
+            if size > max_filesize then
+              vim.b.large_buf = true
+              return true
+            end
 
-        -- check column lengths
-        local lines = vim.api.nvim_buf_get_lines(0, 0, -1, false)
-        for _, line in ipairs(lines) do
-          if #line > vim.o.synmaxcol then
-            vim.b.large_buf = true
-            return true
-          end
-        end
+            -- check column lengths
+            local lines = vim.api.nvim_buf_get_lines(0, 0, -1, false)
+            for _, line in ipairs(lines) do
+              if #line > vim.o.synmaxcol then
+                vim.b.large_buf = true
+                return true
+              end
+            end
 
-        vim.o.foldmethod = 'expr'
-      end,
-    },
-    indent = {
-      enable = true,
-    },
-    autotag = {
-      enable = true,
-    },
-  })
+            vim.o.foldmethod = 'expr'
+          end,
+        },
+        indent = {
+          enable = true,
+        },
+        autotag = {
+          enable = true,
+        },
+      })
 
-  set('n', '[c', require('treesitter-context').go_to_context)
-end
+      set('n', '[c', require('treesitter-context').go_to_context)
+    end
+  end,
+})
 -- }}}
 
 -- spzenhan {{{
