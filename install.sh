197d719844b770367f324c7e0869977438b71ae3
Foo-x <www.sand.x@gmail.com>
2024-11-05 02:05:48 +0900

    fix: install.sh

--- install.sh
+++ install.sh
@@ -258,10 +258,12 @@ else
 fi
 
 # setup vim
+mkdir -p ${XDG_CONFIG_HOME}/vim
 vimrc_files=$(cd ${DOT_DIR}/.config/vim && \ls -1 *vimrc)
 printf "${vimrc_files}" | xargs -I{} ln -sf ${DOT_DIR}/.config/vim/{} ${XDG_CONFIG_HOME}/vim/{}
 
 # setup nvim
+mkdir -p ${XDG_CONFIG_HOME}/nvim
 nvim_files=$(cd ${DOT_DIR}/.config/nvim && \ls -1)
 printf "${nvim_files}" | xargs -I{} ln -sfT ${DOT_DIR}/.config/nvim/{} ${XDG_CONFIG_HOME}/nvim/{}
 
