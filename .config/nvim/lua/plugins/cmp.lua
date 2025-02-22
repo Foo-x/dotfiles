350b65d0e725d8c80c5d29fccf8c675b15acc913
Foo-x <www.sand.x@gmail.com>
2025-02-22 22:01:22 +0900

    chore: update some
    - vim conceallevel
    - vim cmp config

--- .config/nvim/lua/plugins/cmp.lua
+++ .config/nvim/lua/plugins/cmp.lua
@@ -2,11 +2,6 @@ local function cmp_config()
   local cmp = require('cmp')
   local lspkind = require('lspkind')
 
-  local has_words_before = function()
-    local line, col = unpack(vim.api.nvim_win_get_cursor(0))
-    return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match('%s') == nil
-  end
-
   local feedkey = function(key, mode)
     vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes(key, true, true, true), mode, true)
   end
@@ -39,8 +34,6 @@ local function cmp_config()
               cmp.select_next_item({ behavior = cmp.SelectBehavior.Select })
             elseif vim.fn['vsnip#jumpable'](1) == 1 then
               feedkey('<Plug>(vsnip-jump-next)', '')
-            elseif has_words_before() then
-              cmp.complete()
             else
               fallback()
             end
@@ -52,6 +45,8 @@ local function cmp_config()
               cmp.select_prev_item()
             elseif vim.fn['vsnip#jumpable'](-1) == 1 then
               feedkey('<Plug>(vsnip-jump-prev)', '')
+            else
+              fallback()
             end
           end,
         }),
