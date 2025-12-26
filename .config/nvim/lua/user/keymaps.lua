e92e95a364e75763f0b09f78bbff8b3bd6c90ff2
Foo-x <www.sand.x@gmail.com>
2025-12-27 04:01:27 +0900

    chore: update some

--- .config/nvim/lua/user/keymaps.lua
+++ .config/nvim/lua/user/keymaps.lua
@@ -5,6 +5,7 @@ end, { silent = true })
 vim.api.nvim_create_autocmd('FileType', {
   pattern = 'man',
   callback = function(ev)
+    vim.keymap.set('n', 'q', "<Cmd>q<CR>", { buffer = ev.buf, silent = true })
     vim.keymap.set('n', 'gO', "<Cmd>lua require'man'.show_toc()<CR>", { buffer = ev.buf, silent = true })
     vim.keymap.set('n', '<CR>', '<C-]>', { buffer = ev.buf, silent = true })
   end,
